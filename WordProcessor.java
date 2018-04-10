de8e99fa322a0833d295f5acab213a6a7020b140
Mostafa <mwhassan@wisc.edu>
2018-04-10 17:34:34 -0500

    Finished WordProcessor class

--- WordProcessor.java
+++ WordProcessor.java
@@ -64,8 +64,11 @@ public class WordProcessor {
 		 * Note: since map and filter return the updated Stream objects, they can chained together as:
 		 * 		streamOfLines.map(...).filter(a -> ...).map(...) and so on
 		 */
-		
-		return null;
+		Stream<String> wordStream = Files.lines(Paths.get(filepath));
+		wordStream.map(String::trim);
+		wordStream.filter(x -> x!=null && !x.equals(""));
+		wordStream.map (String::toUpperCase);
+		return wordStream;
 	}
 	
 	/**
