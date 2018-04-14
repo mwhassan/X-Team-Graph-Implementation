8297614054f78d6e33e630376b6659ec9cbb87a2
MaggieGuo0914 <36905751+MaggieGuo0914@users.noreply.github.com>
2018-04-13 22:40:01 -0500

    Update GraphProcessorTest.java
    Tests for populateGraph, getShortestPath, and getShortestDistance
    Still working on precomputation

--- GraphProcessorTest.java
+++ GraphProcessorTest.java
@@ -1,6 +1,5 @@
 import static org.junit.Assert.fail;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.After;
@@ -53,9 +52,128 @@ public class GraphProcessorTest {
 		}
 	}
 	
+	/**
+	 * fails if getShortestDistance between "streak" and "husband" doesn't return -1
+	 */
 	@Test
-	public final void populateGraph_set_correct_edges_unreachable() {
-		
+	public final void getShortestDistance_between_words_unreachable() {
+		graphProc.populateGraph("GraphProcessorTestFile.txt");
+		int act = graphProc.getShortestDistance("streak", "husband");
+		int exp = -1;
+		if (exp != act) {
+			fail("expected:" + exp + "actual:" + act);
+		}
 	}
+	
+	@Test
+	public final void getShortestDistance_between_word_and_itself() {
+		graphProc.populateGraph("GraphProcessorTestFile.txt");
+		int act = graphProc.getShortestDistance("hot", "hot");
+		int exp = -1;
+		if (exp != act) {
+			fail("expected:" + exp + "actual:" + act);
+		}
+	}
+	
+	@Test
+	public final void getShortestDistance_between_words_replacement() {
+		graphProc.populateGraph("GraphProcessorTestFile.txt");
+		int act = graphProc.getShortestDistance("hot", "hat");
+		int exp = 1;
+		if (exp != act) {
+			fail("expected:" + exp + "actual:" + act);
+		}
+	}
+	
+	@Test
+	public final void getShortestDistance_between_words_addition() {
+		graphProc.populateGraph("GraphProcessorTestFile.txt");
+		int act = graphProc.getShortestDistance("hat", "heat");
+		int exp = 1;
+		if (exp != act) {
+			fail("expected:" + exp + "actual:" + act);
+		}
+	}
+	
 
+	@Test
+	public final void getShortestDistance_between_words_deletion() {
+		graphProc.populateGraph("GraphProcessorTestFile.txt");
+		int act = graphProc.getShortestDistance("hat", "at");
+		int exp = 1;
+		if (exp != act) {
+			fail("expected:" + exp + "actual:" + act);
+		}
+	}
+	
+	@Test
+	public final void getShortestDistance_between_words_reachable() {
+		graphProc.populateGraph("GraphProcessorTestFile.txt");
+		int act = graphProc.getShortestDistance("it", "wheat");
+		int exp = 4;
+		if (exp != act) {
+			fail("expected:" + exp + "actual:" + act);
+		}
+	}
+	
+	@Test
+	public final void getShortestPath_between_words_unreachable() {
+		graphProc.populateGraph("GraphProcessorTestFile.txt");
+		String act = graphProc.getShortestDistance("streak", "husband").toString();
+		String exp = "[]";
+		if (!act.equals(exp)) {
+			fail("expected:" + exp + "actual:" + act);
+		}
+	}
+	
+	@Test
+	public final void getShortestPath_between_word_and_itself() {
+		graphProc.populateGraph("GraphProcessorTestFile.txt");
+		String act = graphProc.getShortestDistance("hot", "hot").toString();
+		String exp = "[]";
+		if (!act.equals(exp)) {
+			fail("expected:" + exp + "actual:" + act);
+		}
+	}
+	
+	@Test
+	public final void getShortestPath_between_words_replacement() {
+		graphProc.populateGraph("GraphProcessorTestFile.txt");
+		String act = graphProc.getShortestDistance("hot", "hat").toString();
+		String exp = "[hot, hat]";
+		if (!act.equals(exp)) {
+			fail("expected:" + exp + "actual:" + act);
+		}
+	}
+	
+	@Test
+	public final void getShortestPath_between_words_addition() {
+		graphProc.populateGraph("GraphProcessorTestFile.txt");
+		String act = graphProc.getShortestDistance("hat", "heat").toString();
+		String exp = "[hat, heat]";
+		if (!act.equals(exp)) {
+			fail("expected:" + exp + "actual:" + act);
+		}
+	}
+	
+
+	@Test
+	public final void getShortestPath_between_words_deletion() {
+		graphProc.populateGraph("GraphProcessorTestFile.txt");
+		String act = graphProc.getShortestDistance("hat", "at").toString();
+		String exp = "[hat, at]";
+		if (!act.equals(exp)) {
+			fail("expected:" + exp + "actual:" + act);
+		}
+	}
+	
+	@Test
+	public final void getShortestPath_between_words_reachable() {
+		graphProc.populateGraph("GraphProcessorTestFile.txt");
+		String act = graphProc.getShortestDistance("it", "wheat").toString();
+		String exp = "[it, at, hat, heat, wheat]";
+		if (!act.equals(exp)) {
+			fail("expected:" + exp + "actual:" + act);
+		}
+	}
 }
